a15763fa30780142242b6f28fc450ab726cf67ab
Aryankchauhan2722 <aryan.k.chauhan2722@gmail.com>
2026-06-27 14:51:27 +0530

    Update api.js
    Updated API URLS

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://aryanportfolio-backend.onrender.com/api'
 })
 
 export const getProjects = () => API.get('/projects')
@@ -13,5 +13,5 @@ export const sendContact = (data) => API.post('/contact', data)
 export const getSettings = () => API.get('/settings')
 
 export const sendChatMessage = (message) => {
-    return axios.post('http://localhost:5001/chat' , {message})
+    return axios.post('https://aryanportfolio-chatbot-backend.onrender.com/chat' , {message})
 }
